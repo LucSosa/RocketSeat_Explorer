b988701b0fb60f039bab5ea1908e27c34928c7f5
Lucas Sosa Machado <84147924+LucSosa@users.noreply.github.com>
2023-01-13 06:20:47 -0300

    feat: commit stage 07
    this stage is about nodeJS

---
+++ stage_07/api/src/controllers/UsersController.js
@@ -0,0 +1,23 @@
+const AppError = require("../utils/AppError");
+
+class UsersController {
+  create(request, response) {
+    const { name, email, password } = request.body;
+
+    if (!name) {
+      throw new AppError("Nome é obrigatório!");
+    }
+
+    response.status(201).json({ name, email, password });
+  }
+}
+
+module.exports = UsersController;
+
+/**
+ * index - GET para listar vários registros
+ * show - GET para exibir um registro especifico
+ * create - POST para criar um registro
+ * update - PUT para atualizar um registro
+ * delete - DELETE para remover um registro
+ */
